d4179ed8ac51d4822a4c5d9dd519ee2ebfa0ec94
montecillodavid <montecillodavid@gmail.com>
2014-01-08 20:38:12 +0800

    updated splash activity

--- src/com/createconvertupdates/media/SplashActivity.java
+++ src/com/createconvertupdates/media/SplashActivity.java
@@ -27,6 +27,7 @@ import android.view.View;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -60,7 +61,12 @@ public class SplashActivity extends Activity implements OnTouchListener{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
 		setContentView(R.layout.activity_splash_screen);
+		
+		
 		
 		if(checkPlayServices()){
 			Customer customer = getSavedCustomer(SplashActivity.this);
